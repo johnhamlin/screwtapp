cc4baf4c1cd9b2e8869e32e8ede0818c857eca11
John Hamlin <johnhamlin@gmail.com>
2023-06-12 14:09:32 -0400

    Add RTK Query endpoint to fetch mixtape metadata

--- app/mixtape/[identifier]/index.tsx
+++ app/mixtape/[identifier]/index.tsx
@@ -1,14 +1,28 @@
-import { NativeBaseProvider, Text, Box } from 'native-base';
-import { Link, useLocalSearchParams } from 'expo-router';
+import { NativeBaseProvider, Text, Box, Button } from 'native-base';
+import { Link, useLocalSearchParams, useRouter } from 'expo-router';
+import { useGetMixtapeMetadataQuery } from '../../../model/redux/services/mixtapeList';
 
 export default function MixtapeDetails() {
+  const router = useRouter();
   const { identifier } = useLocalSearchParams();
+  const { data } = useGetMixtapeMetadataQuery(identifier as string);
 
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>{identifier}</Text>
+      <Box
+        flex={1}
+        bg="#fff"
+        alignItems="center"
+        justifyContent="center"
+        safeArea
+      >
+        <Text fontSize={50}>BUMP</Text>
+
+        <Button onPress={() => router.push('/')}>Home</Button>
+
         <Link href={'/'}>Home</Link>
+        <Text>{identifier}</Text>
+        <Text>{JSON.stringify(data)}</Text>
       </Box>
     </NativeBaseProvider>
   );
--- model/redux/services/mixtapeList.ts
+++ model/redux/services/mixtapeList.ts
@@ -2,6 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const COLLECTION_URL =
   'https://archive.org/services/search/v1/scrape?fields=title,date,identifier,downloads,creator&q=collection:dj-screw-discography';
 
+interface MixtapeListResponse {
+  items: Mixtape[];
+  count: number;
+  total: number;
+}
+
 export const mixtapeListApi = createApi({
   reducerPath: 'mixtapeListApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://archive.org/' }),
@@ -9,8 +15,14 @@ export const mixtapeListApi = createApi({
     getMixtapeList: builder.query({
       query: () =>
         'services/search/v1/scrape?fields=title,date,identifier,downloads,creator&q=collection:dj-screw-discography',
+      transformResponse: (response: MixtapeListResponse, meta, arg) =>
+        response.items,
+    }),
+    getMixtapeMetadata: builder.query({
+      query: (identifier: string) => `metadata/${identifier}`,
     }),
   }),
 });
 
-export const { useGetMixtapeListQuery } = mixtapeListApi;
+export const { useGetMixtapeListQuery, useGetMixtapeMetadataQuery } =
+  mixtapeListApi;
