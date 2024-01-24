512736f63bc2008b1af668c28eb9e8140e56e651
John Hamlin <johnhamlin@gmail.com>
2024-01-23 20:59:55 -0500

    Configure Dark Mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,24 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../model/redux/store';
 import { Slot } from 'expo-router';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useColorScheme } from 'react-native';
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
 
 export default function Layout() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+  console.log({ colorScheme });
+
+  const paperTheme =
+    colorScheme === 'dark'
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
+
   return (
     <ReduxProvider store={store}>
-      <PaperProvider>
+      <PaperProvider theme={paperTheme}>
         <SafeAreaProvider>
           <Slot />
         </SafeAreaProvider>
